a81070f66d79fa890e75a97eab9143eaef1eb3b0
sabuj_kumar <sabuj.job21@gmail.com>
2023-09-12 23:59:48 +0600

    post entity added successfully

--- src/main/java/com/backend/blog/entity/Category.java
+++ src/main/java/com/backend/blog/entity/Category.java
@@ -1,10 +1,16 @@
 package com.backend.blog.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,10 +26,21 @@ public class Category {
 	
 	@Column(name="discription")
 	private String discription;
-
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	List<Post> posts = new ArrayList<Post>();
+	
 	public Category() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	public Integer getCategoryId() {
--- src/main/java/com/backend/blog/entity/User.java
+++ src/main/java/com/backend/blog/entity/User.java
@@ -1,10 +1,16 @@
 package com.backend.blog.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -18,10 +24,22 @@ public class User {
 	
 	@Column(name="user_name", nullable=false,length=100)
 	private String name;
+	
 	private String email;
+	
 	private String password;
+	
 	private String about;
 	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	List<Post> posts = new ArrayList<Post>();
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 	public int getId() {
 		return id;
 	}
@@ -52,5 +70,4 @@ public class User {
 	public void setAbout(String about) {
 		this.about = about;
 	}
-	
 }
